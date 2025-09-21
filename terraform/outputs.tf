f24a56f3317d52144eab05ba975ca6b1732db0fd
Shivam Patel <shivamdpat94@gmail.com>
2025-09-21 10:58:07 -0400

    importing to intellij

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "docs_bucket_name" {
 output "lambda" {
   value = aws_lambda_function.app.function_name
 }
+
+# terraform/outputs.tf
+output "app_lambda_name" {
+  value = aws_lambda_function.app.function_name
+}
